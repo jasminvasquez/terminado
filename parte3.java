32d4d112b593a9f81c4a335157402aa676857c0d
jasminvasquez <deysijasmin@unitec.com>
2014-03-20 19:21:30 -0600

    Update parte3.java

--- parte3.java
+++ parte3.java
@@ -5,15 +5,19 @@ public class parte3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		  Scanner ab=new Scanner(System.in);
-	        int sum,f,valor,prom;
+	
+	  Scanner mi_Scanner=new Scanner(System.in);
+	        int sum;
+	        int a;
+	        int n;
+	        int prom;
 	        sum=0;
-	        for(f=1;f<=20;f++) {
+	        for(a=1;a<=20;a++) {
 	            System.out.print("Ingrese numero:");
-	            valor=ab.nextInt();
-	            sum=sum+valor;
+	            n=mi_Scanner.nextInt();
+	            sum=sum+n;
 	        }
-	       
+
 	        prom=sum/10;
 	        System.out.print("el total de promedio de todos los numeros es:");
 	        System.out.print(prom);
